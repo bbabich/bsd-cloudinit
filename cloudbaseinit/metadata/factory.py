85d691d18dd4c2914a425d860815a4473ca377be
bbabich <ben@activeservices.net.au>
2017-05-08 21:45:16 +1000

    Update factory.py
    Default to 'EC2 Metadata' source.

--- cloudbaseinit/metadata/factory.py
+++ cloudbaseinit/metadata/factory.py
@@ -22,9 +22,9 @@ opts = [
     cfg.ListOpt(
         'metadata_services',
         default=[
-            'cloudbaseinit.metadata.services.httpservice.HttpService',
+            #'cloudbaseinit.metadata.services.httpservice.HttpService',
             #'cloudbaseinit.metadata.services.configdrive.ConfigDriveService',
-            #'cloudbaseinit.metadata.services.ec2service.EC2Service',
+            'cloudbaseinit.metadata.services.ec2service.EC2Service',
             #'cloudbaseinit.metadata.services.maasservice.MaaSHttpService',
             #'cloudbaseinit.metadata.services.cloudstack.CloudStack',
             #'cloudbaseinit.metadata.services'
